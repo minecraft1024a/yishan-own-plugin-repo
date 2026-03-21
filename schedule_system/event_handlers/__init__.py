53a55e22622da50bebc872e47b8095f3e88f8bd4
minecraft1024a <wwwww95915@qq.com>
2026-03-21 19:46:01 +0800

    del schedule

--- schedule_system/event_handlers/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Event Handler 层
-
-处理系统事件，注册定时任务。
-"""
-
-from .schedule_init_handler import ScheduleInitHandler
-
-__all__ = ["ScheduleInitHandler"]
--- schedule_system/services/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Service 层
-
-对外暴露标准化的服务接口，供其他插件调用。
-"""
-
-from .schedule_service import ScheduleService
-from .plan_service import PlanService
-
-__all__ = ["ScheduleService", "PlanService"]
